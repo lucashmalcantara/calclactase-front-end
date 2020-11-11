28527e1f03d07a0a49090e459dd5255941767e53
Lucas Alcantara <lucashmalcantara@gmail.com>
2020-11-11 13:00:12 -0300

    Melhorias na lista de produtos.

--- calclactase/src/components/Product.js
+++ calclactase/src/components/Product.js
@@ -13,32 +13,18 @@ export default function Product(props) {
   // };
 
   return (
-    <ListItem>
-      <Image
-        style={styles.tinyProductImage}
-        source={Logo}
-      />
-      <View style={{ flex: 1 }}>
-        <View style={styles.productInfoContainer}>
-          <Text style={styles.productInfo}>{props.name}</Text>
-          <Text style={styles.productInfo}>{props.displayUnitValue} {props.displayUnit}</Text>
-        </View>
-
-        <View style={{ flexDirection: 'row', alignSelf: 'flex-end' }}>
-          <Button small danger style={styles.button} onPress={() => props.onRemove(props.id)}>
-            <Icon type="FontAwesome" name="minus" />
-          </Button>
-          {/* <Input
-              style={styles.input}
-              defaultValue={props.quantity}
-              // onChangeText={(campo) => setCampo(campo)}
-              // onSubmitEditing={confirma}
-              autoFocus
-            /> */}
-          <Button small info style={styles.button} onPress={() => props.onAdd(props.id)}>
-            <Icon type="FontAwesome" name="plus" />
-          </Button>
-        </View>
+    <ListItem style={{ alignItems: 'flex-start' }}>
+      <View style={styles.productInfoContainer}>
+        <Text style={styles.productInfo}>{props.name}</Text>
+        <Text style={styles.productInfo}>{props.displayUnitValue} {props.displayUnit}</Text>
+      </View>
+      <View style={{ flexDirection: 'column', alignItems: 'center' }}>
+        <Image
+          style={styles.tinyProductImage}
+          source={Logo} />
+        <Button small info style={styles.button} onPress={() => props.onAdd(props.id)}>
+          <Text>ADICIONAR</Text>
+        </Button>
       </View>
     </ListItem>
   );
@@ -50,6 +36,7 @@ const styles = StyleSheet.create({
   },
   productInfoContainer: {
     flex: 1,
+    alignItems: 'flex-start',
     marginLeft: 16
   },
   productInfo: {
@@ -64,6 +51,7 @@ const styles = StyleSheet.create({
   },
   button: {
     marginLeft: 5,
+    marginTop: 16
   },
   tinyProductImage: {
     height: 60,
