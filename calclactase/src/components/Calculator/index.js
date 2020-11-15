4e8522e5e9460b22249c16e25108c2234047e465
Lucas Alcantara <lucashmalcantara@gmail.com>
2020-11-14 23:48:12 -0300

    Exibição de informação de calculadora vazia.

--- calclactase/src/components/Calculator/index.js
+++ calclactase/src/components/Calculator/index.js
@@ -1,28 +1,41 @@
 import React from "react";
-import { StyleSheet } from "react-native";
-import { H1, List } from "native-base";
+import { StyleSheet, View, Image } from "react-native";
+import { H1, List, Text } from "native-base";
 import CalculatorItem from "../CalculatorItem";
 import styles from "./styles";
+import BagImage from "../../assets/images/shopping-bag.png";
 
 export default function Calculator(props) {
   return (
     <React.Fragment>
-      <H1 style={styles.largeTitle}>Itens da calculadora</H1>
-      <List>
-        {props.items.map((item) => (
-          <CalculatorItem
-            key={item.id}
-            id={item.id}
-            name={item.product.name}
-            displayUnitValue={item.product.displayUnitValue}
-            displayUnit={item.product.displayUnit}
-            quantity={item.quantity}
-            imageUrl={item.product.imageUrl}
-            onAdd={props.onAdd}
-            onRemove={props.onRemove}
-          />
-        ))}
-      </List>
+      {props.items.length === 0 ? (
+        <View style={styles.emptyCalculatorContainer}>
+          <Image source={BagImage} />
+          <Text style={styles.emptyCalculatorText}>
+            Não existem itens na calculadora, adicione para calcular a
+            quantidade de lactase.
+          </Text>
+        </View>
+      ) : (
+        <React.Fragment>
+          <H1 style={styles.largeTitle}>Itens da calculadora</H1>
+          <List>
+            {props.items.map((item) => (
+              <CalculatorItem
+                key={item.id}
+                id={item.id}
+                name={item.product.name}
+                displayUnitValue={item.product.displayUnitValue}
+                displayUnit={item.product.displayUnit}
+                quantity={item.quantity}
+                imageUrl={item.product.imageUrl}
+                onAdd={props.onAdd}
+                onRemove={props.onRemove}
+              />
+            ))}
+          </List>
+        </React.Fragment>
+      )}
     </React.Fragment>
   );
 }
--- calclactase/src/components/Calculator/styles.js
+++ calclactase/src/components/Calculator/styles.js
@@ -1,8 +1,22 @@
-import { StyleSheet } from 'react-native'
-import { general } from "../../styles";
+import { StyleSheet } from "react-native";
+import { general, metrics } from "../../styles";
 
 const styles = StyleSheet.create({
   ...general,
+  emptyCalculatorContainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 125,
+  },
+  emptyCalculatorImage: {
+    height: 120,
+    width: 120,
+    resizeMode: "contain",
+  },
+  emptyCalculatorText: {
+    textAlign: "center",
+  },
 });
 
 export default styles;
